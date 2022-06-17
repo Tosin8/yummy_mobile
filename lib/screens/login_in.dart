aba4a5cd4763a2ec81703738b02b0cc2882f4a27
Tosin8 <tosinezekiel8@gmail.com>
2022-06-17 18:21:35 +0100

    updating the login page

--- lib/screens/login_in.dart
+++ lib/screens/login_in.dart
@@ -5,11 +5,46 @@ class LoginIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          
-
-        ]));
+    return Stack(children: [
+      ShaderMask(
+          shaderCallback: (rect) => const LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.center,
+                colors: [Colors.black, Colors.transparent],
+              ).createShader(rect),
+          blendMode: BlendMode.darken,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+              ),
+            ),
+          )),
+      Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(children: [
+            Flexible(
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            const SizedBox(height: 4),
+            const Text('Welcome back User!',
+                style: TextStyle(color: Colors.white, fontSize: 20)),
+            const SizedBox(height: 20),
+            Container(
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Colors.grey[500].withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: const TextField(
+                    decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Email',
+                  hintStyle: TextStyle(fontSize: 22, color: Colors.white),
+                )))
+          ]))
+    ]);
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:master_chef_app_form/main.dart';
+import 'package:master_chef_app_form/screens/login_in.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const LandingPage());
+    await tester.pumpWidget(const LoginIn());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
