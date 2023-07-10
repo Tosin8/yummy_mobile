9c28b85319524d9633a6a1e078272e195c3a4f6e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-10 20:02:39 +0100

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,70 +1,40 @@
-
-
-
 import 'package:flutter/material.dart';
 
 import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
-
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
-
   State<SplashScreen> createState() => _SplashScreenState();
-
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
-
   void initState() {
-
     super.initState();
 
     Future.delayed(
-
       const Duration(seconds: 3),
-
       () {
-
         Navigator.push(
-
           context,
-
           MaterialPageRoute(
-
             builder: (context) => const Login(),
-
           ),
-
         );
-
       },
-
     );
-
   }
 
   @override
-
   Widget build(BuildContext context) {
-
     return const Scaffold(
-
       body: Center(
-
         child: FlutterLogo(
-
           size: 200,
-
         ),
-
       ),
-
     );
-
   }
-
 }
