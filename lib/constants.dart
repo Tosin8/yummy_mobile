c0d8d797763f0259e36657cfd38a7b8cbfcabf10
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-25 02:03:34 +0100

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xffFFE157);
+const kPrimaryColor = Color(0xffe2cb5d);
 const kSecondaryColor = Color(0xffFF8D57);
 const kAccentColor = Color(0xFFC9FF57);
