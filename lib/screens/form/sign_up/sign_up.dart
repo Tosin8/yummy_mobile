04807dfb16d9b48f3d0c7263f32601111bf4518d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-26 22:46:47 +0100

    Updating with SingleChildScrollView

--- lib/screens/form/sign_up/sign_up.dart
+++ lib/screens/form/sign_up/sign_up.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../log_in/login_in.dart';
+import 'package:yummy_mobile/constants.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({super.key});
+  const SignUp({Key? key}) : super(key: key);
 
   @override
   State<SignUp> createState() => _SignUpState();
@@ -15,14 +13,27 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-          height:  double.infinity, 
-          width: double.infinity, 
-          ), 
-        ],
-      )
+      body: Stack(children: [
+        Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: kBkgColor,
+            )),
+        Container(
+          height: double.infinity,
+          child: const SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 120.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
