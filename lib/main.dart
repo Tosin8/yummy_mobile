0728e161c0c536f77f0a5be16cb93cd60dcda375
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-26 12:15:23 +0100

    Creating OTP Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'screens/splash_screen.dart';
+import 'screens/form/verification_page.dart';
 
 void main() {
   runApp(const MaterialApp(
-      title: 'Master Chef',
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen()));
+      title: 'Master Chef', debugShowCheckedModeBanner: false, home: OTP()));
 }
--- lib/screens/form/verification_page.dart
+++ lib/screens/form/verification_page.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class OTP extends StatelessWidget {
+  const OTP({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
