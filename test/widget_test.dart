f529bd5ae1ad68ebb2691bb0df68de5b32ef76a5
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-26 10:51:04 +0100

    Updating the form page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:yummy_mobile/screens/log_in/login_in.dart';
+import 'package:yummy_mobile/screens/form/log_in/login_in.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
