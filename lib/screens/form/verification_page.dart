fce1913facd4a86c89b98bfad672b8935c93fb1d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-26 12:25:32 +0100

    Update verification_page.dart

--- lib/screens/form/verification_page.dart
+++ lib/screens/form/verification_page.dart
@@ -5,6 +5,38 @@ class OTP extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 64,
+            width: 68,
+            child: TextFormField(
+              autofocus: true,
+              onSaved: (pin1) {},
+              keyboardType: TextInputType.number,
+              maxLength: 1,
+              decoration: const InputDecoration(counterText: ""),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          SizedBox(
+            height: 64,
+            width: 68,
+            child: TextFormField(
+              autofocus: true,
+              onSaved: (pin1) {},
+              keyboardType: TextInputType.number,
+              maxLength: 1,
+              decoration: const InputDecoration(counterText: ""),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
