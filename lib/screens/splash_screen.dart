ebb1d6a479e94f6146106d800c3366897b6332c9
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-26 16:16:32 +0100

    Update splash_screen.dart
    Updating with Image logo splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -36,85 +36,11 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      width: 390,
-      height: 844,
-      clipBehavior: Clip.antiAlias,
-      decoration: const BoxDecoration(color: Colors.white),
-      child: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            top: 0,
-            child: Container(
-              width: 390,
-              height: 844,
-              decoration: const BoxDecoration(color: kPrimaryColor),
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
             ),
-          ),
-          Positioned(
-            left: 148,
-            top: 280,
-            child: Container(
-              width: 87,
-              height: 202,
-              child: Stack(
-                children: [
-                  const Positioned(
-                    left: 19,
-                    top: 0,
-                    child: SizedBox(
-                      width: 64,
-                      height: 66,
-                      child: Text(
-                        'y',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 128,
-                          fontFamily: 'Akaya Telivigala',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 64,
-                    top: 38,
-                    child: Container(
-                      width: 19,
-                      height: 19,
-                      decoration: const ShapeDecoration(
-                        color: Colors.black,
-                        shape: OvalBorder(),
-                        shadows: [
-                          BoxShadow(
-                            color: Color(0x3F000000),
-                            blurRadius: 4,
-                            offset: Offset(0, 4),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Positioned(
-                    left: 0,
-                    top: 184,
-                    child: Text(
-                      'belle’s friend...',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontFamily: 'Akaya Telivigala',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    ));
+            child: Center(Image.asset(
+              'assets/images/logo.png',
+            ))));
   }
 }
