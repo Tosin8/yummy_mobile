72e1ce1a922bcec288e4fb42474aaaa531ef4db1
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-26 19:58:54 +0100

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'dart:async';
-
 import '../constants.dart';
 import 'package:lottie/lottie.dart';
+
+import 'welcome_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,22 +13,22 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  //@override
-  // void initState() {
-  //   super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-  //   Future.delayed(
-  //     const Duration(seconds: 15),
-  //     () {
-  //       Navigator.push(
-  //         context,
-  //         MaterialPageRoute(
-  //           builder: (context) => const WelcomePage(),
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
+    Future.delayed(
+      const Duration(seconds: 10),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const WelcomePage(),
+          ),
+        );
+      },
+    );
+  }
 
   // TODO: making the text to fade in, then bounce up and down before moving to the next page, also adding responsive page elements.
 
@@ -46,9 +46,9 @@ class _SplashScreenState extends State<SplashScreen> {
               height: 150,
               width: 150,
             ),
-            const SizedBox(height: 80),
+            const SizedBox(height: 100),
             Lottie.asset('assets/icons/animation_lkjzljaq.json',
-                height: 70, repeat: true, reverse: true, animate: true),
+                height: 50, repeat: true, reverse: true, animate: true),
           ]),
         ),
       ),
