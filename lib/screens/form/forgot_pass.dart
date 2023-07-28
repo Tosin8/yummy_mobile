cb95cbf14bd0b31818c0875d93f0c777548ce44c
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-29 00:49:11 +0100

    Update forgot_pass.dart

--- lib/screens/form/forgot_pass.dart
+++ lib/screens/form/forgot_pass.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
+import 'log_in/widgets.dart';
 
 class ForgottenPassword extends StatefulWidget {
   const ForgottenPassword({Key? key}) : super(key: key);
@@ -36,16 +37,44 @@ class _ForgottenPasswordState extends State<ForgottenPassword> {
                         child: Image.asset(
                             'assets/images/product_icon/logo_sub.png')),
                     const SizedBox(height: 20),
-                    const Column(
+                    Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Reset Password', style: kMainFormHeaderStyle),
-                          SizedBox(height: 5),
-                          Text(
+                          const Text('Reset Password',
+                              style: kMainFormHeaderStyle),
+                          const SizedBox(height: 5),
+                          const Text(
                             'Kindy enter your registered email',
                             style: kSubTextStyle,
                           ),
-                          SizedBox(height: 10),
+                          const SizedBox(height: 10),
+                          EmailLabel,
+                          const SizedBox(height: 30),
+                          GestureDetector(
+                            onTap: () {
+                              print('submit button has been tapped');
+                            },
+                            child: Center(
+                              child: Container(
+                                width: 200,
+                                height: 45,
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 12.0),
+                                decoration: BoxDecoration(
+                                    color: kPrimaryColor,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                  child: Text(
+                                    'Send Code',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ])
                   ])))
     ]));
