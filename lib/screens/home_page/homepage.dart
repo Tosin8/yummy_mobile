76528f28b99b02916df444c3c7d917f71df7fcb6
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-30 17:27:35 +0100

    Update homepage.dart

--- lib/screens/home_page/homepage.dart
+++ lib/screens/home_page/homepage.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-  
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       bottomNavigationBar: Container(
-        
+        margin: const EdgeInsets.all(20),
+        height: size.width * .155,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(.15),
+              blurRadius: 30,
+              offset: const Offset(0, 10),
+            )
+          ],
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ListView.builder(itemBuilder: itemBuilder),
       ),
     );
   }
