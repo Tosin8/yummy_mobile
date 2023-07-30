bde72f9ad4e20293bde4ab94fbfdff55880bd1d7
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-07-30 17:22:07 +0100

    Updating with BottomNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:yummy_mobile/rough.dart';
-import 'package:yummy_mobile/screens/splash_screen.dart';
 
 import 'screens/home_page/homepage.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
       title: 'Master Chef',
       debugShowCheckedModeBanner: false,
-      home: ComplexAnimation()));
+      home: HomePage()));
 }
--- lib/screens/home_page/homepage.dart
+++ lib/screens/home_page/homepage.dart
@@ -1,50 +1,22 @@
 import 'package:flutter/material.dart';
 
-class ComplexAnimation extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _ComplexAnimationState createState() => _ComplexAnimationState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _ComplexAnimationState extends State<ComplexAnimation>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+class _HomePageState extends State<HomePage> {
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2),
-    );
-    _animation = Tween<double>(begin: 100, end: 300).animate(_controller)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          _controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          _controller.forward();
-        }
-      });
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+  
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return Container(
-          height: _animation.value,
-          width: _animation.value,
-          color: Colors.red,
-        );
-      },
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: Container(
+        
+      ),
     );
   }
 }
