c37576b74e422e076f10a4c7d7bce87b5aa45a4d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-13 23:56:01 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:yummy_mobile/screens/home_page/homepage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomePage());
+    await tester.pumpWidget(const LoginIn());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
